61897705afed6a44aae74e8789a43575add72402
AndersonZX6R <anderson.marques.ribeiro@gmail.com>
2015-05-13 23:39:39 -0300

    General Types
    General types definition

---
+++ TextTools/TextTypes.h
@@ -0,0 +1,13 @@
+#include <string>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+namespace TEXTTOOLS
+{
+
+typedef vector<string> StringListType;
+typedef stack<string> StringStackType;
+
+}; // namespace TEXTTOOLS
