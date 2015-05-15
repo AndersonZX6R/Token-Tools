e7cf0d30494195cda91e2591866e7345839bb845
AndersonZX6R <anderson.marques.ribeiro@gmail.com>
2015-05-15 10:54:42 -0300

    Token Analisys
    Main classes to break strings

---
+++ TextTools/TokenAnalisys.cpp
@@ -0,0 +1,154 @@
+#include "TokenAnalisys.h"
+
+namespace TEXTTOOLS
+{
+
+TokenIdentType
+	lastIdentToken = -1;
+
+/*
+ * TokenAnalisys.
+ * ------------------------------------------------------------------------------------------------------------------------------------------------
+ */
+
+bool
+TokenAnalysis::tokenConfigByIdent(TokenIdentType ident, TokenConfigStruct *config)
+{
+MapTokenConfigType::iterator
+	it = m_mtcTokensConfig.find(ident);
+bool
+	blnResult = it != m_mtcTokensConfig.end();
+
+	if (blnResult && config)
+		*config = it->second;
+
+	return blnResult;
+};
+
+void
+TokenAnalysis::addToken(TokenIdentType ident, const string &token, TokenConfigStruct config)
+{
+    if (m_mtiTokens.find(token) != m_mtiTokens.end())
+        RAISE(ETokenAnalisys, ERR_DUPLICATE_TOKEN);
+
+	if (m_taConfig.uniqueTokenId && tokenConfigByIdent(ident))
+		RAISE(ETokenAnalisys, ERR_DUPLICATE_IDENT);
+
+TokenDataStruct
+	td;
+
+	td.ident = ident;
+	td.str = token;
+
+    m_mtiTokens.insert( pair<string, TokenDataStruct>(token, td) );
+	m_mtcTokensConfig.insert( pair<TokenIdentType, TokenConfigStruct>(ident, config));
+};
+
+long
+TokenAnalysis::breakString(const string &str, TokenDataListType &tdl, bool includeTokens)
+{
+MapTokenDataType::reverse_iterator
+	rit;
+unsigned long
+	lngLastPos = 0,
+	lngPos = 0;
+string
+	strSubStr;
+TokenDataStruct
+	tdAppellant = TOKEN_DATA_NULL;
+
+    while (lngPos < str.length())
+    {
+
+        rit = m_mtiTokens.rbegin();
+        while (rit != m_mtiTokens.rend())
+        {
+        string strToken = rit->first;
+
+            strSubStr = str.substr(lngPos, strToken.length());
+
+            if (strSubStr == strToken)
+            {
+            TokenConfigStruct td = getTokenConfig(strToken);
+            TokenDataStruct tknData;
+
+                if (lngPos - lngLastPos > 0)
+                {
+                    tknData.str = str.substr(lngLastPos, lngPos - lngLastPos);
+                    tknData.ident = TOKEN_IDENT_NULL;
+
+                    tdl.push_back(tknData);
+                };
+
+				lngPos += strToken.length();
+				lngLastPos = lngPos;
+				
+				if (!td.ignore)
+				{
+					// Caso o token tenha configuração para ser recorrente, a pesquisa não será reiniciada nem o token reconhecido será inserido 
+					// ---- na lista de resultados. Com isso, ele será testado novamente até que não haja mais um reconhecimento positivo. Então,
+					//		a porçcão que foi anteriormente reconhecida será adicionada a lista.
+					if (!td.appellant)
+					{
+						if (!td.ignore && includeTokens)
+					    {
+						    tknData.str = strToken;
+							tknData.ident = rit->second.ident;
+
+						    tdl.push_back(tknData);
+		                };
+
+			            rit = m_mtiTokens.rbegin();
+					}
+					else
+					{
+						tdAppellant.str += strToken;
+						tdAppellant.ident = rit->second.ident;
+					};
+				}
+            }
+            else
+			{
+				if (tdAppellant.str.size())
+				{
+					tdl.push_back(tdAppellant);
+					tdAppellant = TOKEN_DATA_NULL;
+				};
+
+                rit++;
+			};
+        };
+
+        lngPos++;
+    };
+
+    strSubStr = str.substr(lngLastPos, lngPos - lngLastPos);
+
+    if (strSubStr.length())
+    {
+    TokenDataStruct tknData;
+
+        tknData.str = strSubStr;
+		tknData.ident = TOKEN_IDENT_NULL;
+
+        tdl.push_back(tknData);
+    };
+
+    return tdl.size();
+};
+
+long
+TokenAnalysis::breakString(const string &str, StringListType &sl, bool includeTokens)
+{
+TokenDataListType tdl;
+long lngResult;
+
+    lngResult = breakString(str, tdl, includeTokens);
+
+    for (TokenDataListType::iterator it = tdl.begin(); it != tdl.end(); it++)
+        sl.push_back(it->str);
+
+    return lngResult;
+};
+
+}; // namespace TEXTTOOLS;
---
+++ TextTools/TokenAnalisys.h
@@ -0,0 +1,172 @@
+/***
+ * TokenAnalisys.h
+ *
+ * Developed by Eng.º Anderson Marques Ribeiro.
+ */
+
+#ifndef __TOKENANALISYS
+#define __TOKENANALISYS
+
+#ifdef _MANAGED
+#pragma unmanaged
+#endif // ifdef _MANAGED
+
+#include <string>
+#include <vector>
+#include <map>
+#include "TextTypes.h"
+#include "..\Utils\Error.h"
+
+#include "..\DataStorage\SymbolTable.h"
+
+using namespace std;
+using namespace DATASTORAGE;
+
+namespace TEXTTOOLS
+{
+
+// v1.1.0
+//
+// Alteração do nome para coincidir com a implementação em Object Pascal.
+// ----
+typedef long TokenIdentType;
+
+// v1.1.0
+//
+// Alteração do nome para coincidir com a implementação em Object Pascal.
+// ----
+const TokenIdentType TOKEN_IDENT_NULL = -1;
+
+extern TokenIdentType lastIdentToken;
+
+#define DECLARE_TOKEN_IDENT(Ident) const TokenIdentType Ident = lastIdentToken++
+
+struct TokenConfigStruct
+{
+    bool ignore, appellant;
+}; // struct TokenConfigStruct
+
+struct TokenDataStruct
+{
+    string str;
+	TokenIdentType ident;
+
+    bool isToken()
+	{
+		return ident != TOKEN_IDENT_NULL;
+	};
+	
+}; // struct TokenDataStruct
+
+typedef vector<TokenDataStruct> TokenDataListType;
+
+const TokenConfigStruct TOKEN_CONFIG_DEFAULT =
+{
+    false, false
+};
+
+const TokenDataStruct TOKEN_DATA_NULL =
+{
+	"", TOKEN_IDENT_NULL
+};
+
+DECLARE_ERROR_CODE(ERR_DUPLICATE_TOKEN);
+DECLARE_ERROR_CODE(ERR_INVALID_TOKEN);
+DECLARE_ERROR_CODE(ERR_DUPLICATE_IDENT);
+
+BEGIN_DECLARE_ERROR(ETokenAnalisys)
+    DECLARE_ERROR(ERR_DUPLICATE_TOKEN, "Token duplicado")
+    DECLARE_ERROR(ERR_INVALID_TOKEN, "Token inválido")
+	DECLARE_ERROR(ERR_DUPLICATE_IDENT, "Identificador de token duplicado")
+END_DECLARE_ERROR;
+
+/***
+ * TokenAnalysisConfigStruct;
+ *
+ * Armazena valores de configuração utilizados por uma instância da classe TokenAnalysis.
+ * ----
+ */
+struct TokenAnalysisConfigStruct
+{
+	// uniqueTokenId;
+	//
+	// Todo token adicionado a lista interna de TokenAnalysis tem um identificador associado a ele. Caso este parâmetro seja configurado como true,
+	// ---- a classe é informada que não deve permitir que um mesmo identificador seja usado por mais de um token.
+	// **************************************************************************************************************************************************
+	bool uniqueTokenId;
+}; // struct TokenAnalysisConfigStruct;
+
+const TokenAnalysisConfigStruct TOKEN_ANALYSIS_CONFIG_PADRAO =
+{
+	false
+};
+
+class TokenAnalysis
+{
+public:
+
+	TokenAnalysis(TokenAnalysisConfigStruct config = TOKEN_ANALYSIS_CONFIG_PADRAO) :
+		m_taConfig(config)
+	{};
+	/*virtual ~TokenAnalysis()
+	{
+		delete m_pDiagrams;
+	};*/
+
+private:
+
+    typedef map<string, TokenDataStruct> MapTokenDataType;
+	typedef map<TokenIdentType, TokenConfigStruct> MapTokenConfigType;
+
+    MapTokenDataType
+		m_mtiTokens;
+	MapTokenConfigType
+		m_mtcTokensConfig;
+
+	TokenAnalysisConfigStruct
+		m_taConfig;
+
+public:
+
+    long getTokenCount() const
+    {
+        return m_mtiTokens.size();
+    };
+
+    TokenConfigStruct getTokenConfig(const string &token)
+    {
+    MapTokenDataType::const_iterator
+		it = m_mtiTokens.find(token);
+
+        if (it == m_mtiTokens.end())
+            RAISE(ETokenAnalisys, ERR_INVALID_TOKEN);
+
+	TokenConfigStruct
+		tc;
+
+		tokenConfigByIdent(it->second.ident, &tc);
+
+		return tc;
+    };
+	bool tokenConfigByIdent(TokenIdentType ident, TokenConfigStruct *config = NULL);
+
+    // addToken;
+    //
+    // Adiciona um novo token na lista interna do analisador.
+    // ----
+    // *****************************************************************************************************************************************
+    void addToken(TokenIdentType ident, const string &token, TokenConfigStruct def = TOKEN_CONFIG_DEFAULT);
+
+	// breakString;
+	//
+	// Decompõe uma string em sub-strings, utilizando os tokens previamente adicionados no objeto como delimitação.
+	// ----
+	// *****************************************************************************************************************************************
+    long breakString(const string &str, TokenDataListType &tdl, bool includeTokens = true);
+    long breakString(const string &str, StringListType &sl, bool includeTokens = true);
+
+}; // class TokenAnalisys
+
+}; // namespace TEXTTOOLS;
+
+#endif // ifndef __TOKENANALISYS
